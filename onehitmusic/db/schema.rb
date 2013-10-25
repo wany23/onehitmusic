e38a823e48e2f60be7bea743005126dd03781071
Brett Clubb <brett.clubb@gmail.com>
2013-10-25 17:15:32 -0500

    del dupes
    deleted duplicate users db

--- onehitmusic/db/schema.rb
+++ onehitmusic/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131024012127) do
+ActiveRecord::Schema.define(:version => 20131024013324) do
 
   create_table "posts", :force => true do |t|
     t.string   "name"
